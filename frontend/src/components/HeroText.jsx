7fbe8c1f546e213f7b3b05b65d8372bdc44591fe
Ayaan Khan <ayaankhan224271@gmail.com>
2026-06-11 19:02:34 +0530

    completed hero text

---
+++ frontend/src/components/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className=' h-[30vw] flex flex-col justify-center items-center text-center gap-3'>
+      <h1 className='text-9xl font-[oran] tracking-tighter'>We make <span className='text-[#E56E3A] font-[nycd]'>talent</span><br />impossible to miss</h1>
+      <p className='text-2xl uppercase'>prodct naam: AI-driven candidate ranking<br /> built for faster, smarter hiring</p>
+    </div>
+  )
+}
+
+export default HeroText
--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroText from '../components/HeroText'
 
 const Hero = () => {
   return (
     <div className='m-6'>
       <Navbar />
+      <HeroText />
     </div>
   )
 }
