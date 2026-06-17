1b60623a63e5a29a2f013f7086e283802f06ca97
Ayaankhan224 <ayaankhan224271@gmail.com>
2026-06-17 14:15:13 +0530

    remove node_modules from git

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const Razorpay = require("razorpay");
+
+dotenv.config();
 
 const rankingRoutes = require("./src/routes/ranking.routes");
 
 const app = express();
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +20,24 @@ app.use(express.json());
 app.use("/api/rank", rankingRoutes);
 
 const PORT = 5000;
+
+app.post("/create-order", async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const order = await razorpay.orders.create({
+      amount: amount * 100,
+      currency: "USD",
+    });
+
+    res.json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Order creation failed",
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
